cd100cc49cce7df4f1e2d7a7f84a05e368dfdfb7
Armani Catlin <k1ngsoap23@gmail.com>
2021-06-15 09:32:29 -0500

    Initial commit

---
+++ src/Board.java
@@ -0,0 +1,9 @@
+public class Board {
+    public void display(){ //set an array value for each of the rows on the board
+        String[] topRow = {"0","0","0"};
+        String[] middleRow = {"0","0","0"};
+        String[] bottomRow = {"0","0","0"};
+    }
+
+    public void 
+}
