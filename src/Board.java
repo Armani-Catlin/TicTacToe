79e0ef33d12314a4b86fcaa6d3fb8232833e2080
Armani Catlin <k1ngsoap23@gmail.com>
2021-06-15 11:11:53 -0500

    First Day Commits

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,41 @@
+import java.util.Scanner;
+import java.util.*;
+
 public class Board {
     public void display(){ //set an array value for each of the rows on the board
-        String[] topRow = {"0","0","0"};
-        String[] middleRow = {"0","0","0"};
-        String[] bottomRow = {"0","0","0"};
+        String[] board; //will be displayed in the format of individual spots, then a line to seperate, then the next spot
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0] + " | "
+                + board[1] + " | " + board[2]
+                + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | "
+                + board[4] + " | " + board[5]
+                + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | "
+                + board[7] + " | " + board[8]
+                + " |");
+        System.out.println("|---|---|---|");
     }
 
-    public void 
+    public void players(){
+        boolean Player1 = true;
+        boolean Player2 = true;
+    }
+
+    public void spotPicker(){
+        int numTurns = 9;
+        String spot;
+        Scanner oneInput = new Scanner(System.in);
+        while(numTurns != 0) {
+            spot = oneInput.nextLine();
+
+            numTurns--;
+        }
+    }
 }
+//Eight total win conditions
+//Draw means number of turns runs out
+//Need to write a case for each win condition
+//
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
 public class Main {
-
+    public static String[] board;
+    static String turns;
+    public static void main(String[] args) {
+        board = new String[9];
+    }
 }
